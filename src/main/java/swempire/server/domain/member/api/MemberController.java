0acade08af25952d96ffad5353ff6a2b2fa23671
황성욱 <tjddnr7760@naver.com>
2023-12-12 13:04:01 +0900

    feat : member controller, service, repository 구현

--- src/main/java/swempire/server/domain/member/api/MemberController.java
+++ src/main/java/swempire/server/domain/member/api/MemberController.java
@@ -1,8 +1,43 @@
 package swempire.server.domain.member.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import swempire.server.domain.member.application.MemberService;
 
+@Slf4j
+@RequestMapping("/member")
+@RequiredArgsConstructor
 @Controller
-public class MemerController {
+public class MemberController {
 
+    private final MemberService memberService;
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "";
+    }
+
+    @PostMapping("/login")
+    public String authLogin() {
+        return "";
+    }
+
+    @PostMapping("/logout")
+    public String authLogout() {
+        return "";
+    }
+
+    @GetMapping
+    public String getSignUpPage() {
+        return "";
+    }
+
+    @PostMapping
+    public String postSignup() {
+        return "";
+    }
 }
--- src/main/java/swempire/server/domain/member/dao/MemberRepository.java
+++ src/main/java/swempire/server/domain/member/dao/MemberRepository.java
@@ -1,4 +1,7 @@
 package swempire.server.domain.member.dao;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import swempire.server.domain.member.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
