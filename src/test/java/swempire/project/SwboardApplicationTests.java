6c714cbd92ed40461d780ae66417375aeea475df
황성욱 <tjddnr7760@naver.com>
2023-12-12 10:13:57 +0900

    feat : 초기 파일 세팅

---
+++ src/test/java/swempire/project/SwboardApplicationTests.java
@@ -0,0 +1,13 @@
+package swempire.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
