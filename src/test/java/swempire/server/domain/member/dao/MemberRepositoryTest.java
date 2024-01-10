71c70b9f5a50ed17eb2f9c2587597b856fb0c1ad
SeongUk Hwang <tjddnr7760@naver.com>
2024-01-10 09:49:28 +0900

    feat : 테스트 코드 작성 준비

---
+++ src/test/java/swempire/server/domain/member/dao/MemberRepositoryTest.java
@@ -0,0 +1,22 @@
+package swempire.server.domain.member.dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+        assertAll();
+
+        // when
+
+
+        //then
+
+    }
+}
